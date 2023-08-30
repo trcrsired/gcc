48eab8fac56d196dc43ebd19e8b753ffe3765820
Zac Walker <zac.walker@linaro.org>
2023-08-30 15:00:28 +0200

    Fix legitimize for aarch64-pe

--- gcc/config/aarch64/cygming.h
+++ gcc/config/aarch64/cygming.h
@@ -132,7 +132,6 @@ extern void i386_pe_seh_init_sections (void);
 extern void aarch64_pe_asm_named_section (const char *, unsigned int, tree);
 extern bool aarch64_pe_valid_dllimport_attribute_p (const_tree);
 extern void aarch64_pe_maybe_record_exported_symbol (tree, const char *, int);
-extern void aarch64_pe_declare_function_type (FILE *, const char *, int);
 
 /* In winnt */
 extern void aarch64_print_reg (rtx, int, FILE*);
@@ -140,6 +139,10 @@ extern void i386_pe_end_function (FILE *f, const char *, tree);
 extern void i386_pe_end_cold_function (FILE *f, const char *, tree);
 extern void aarch64_pe_end_epilogue (FILE *file);
 extern void i386_pe_begin_epilogue (FILE *file);
+extern void i386_pe_file_end (void);
+//extern void aarch64_pe_declare_function_type (FILE *file, const char *name, int pub);
+//extern void aarch64_pe_record_external_function (tree decl, const char *name);
+extern void i386_pe_record_stub (const char *name);
 
 /* Windows64 continues to use a 32-bit long type.  */
 #undef LONG_TYPE_SIZE
@@ -301,4 +304,21 @@ extern void i386_pe_begin_epilogue (FILE *file);
    unwind info for this later allocation.  */
 #define SEH_MAX_FRAME_SIZE ((2U << 30) - 256)
 
+/* Output function declarations at the end of the file.  */
+#undef TARGET_ASM_FILE_END
+#define TARGET_ASM_FILE_END i386_pe_file_end
+
+/* Add an external function to the list of functions to be declared at
+   the end of the file.  */
+/*
+#undef ASM_OUTPUT_EXTERNAL
+#define ASM_OUTPUT_EXTERNAL(FILE, DECL, NAME)				\
+  do									\
+    {									\
+      if (TREE_CODE (DECL) == FUNCTION_DECL)				\
+	aarch64_pe_record_external_function ((DECL), (NAME));		\
+      aarch64_asm_output_external (FILE, DECL, NAME);			\
+    }									\
+  while (0)
+*/
 #endif
